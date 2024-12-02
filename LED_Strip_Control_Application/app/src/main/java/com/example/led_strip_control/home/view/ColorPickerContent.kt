5dc45aa9172c83a73e33fa1e8816ae29a03ac73b
Ahmed Essam <ahmed.e.mogy@gmail.com>
2024-12-02 15:00:04 +0200

    Update ColorPickerContent.kt

--- LED_Strip_Control_Application/app/src/main/java/com/example/led_strip_control/home/view/ColorPickerContent.kt
+++ LED_Strip_Control_Application/app/src/main/java/com/example/led_strip_control/home/view/ColorPickerContent.kt
@@ -39,47 +39,16 @@ fun ColorPickerContent(
 ) {
     val controller = rememberColorPickerController()
     val selectedColor = remember { mutableStateOf(Color(1f, 0f, 0f)) } // Default red color
-
-
-    // List of sample colors for the hint list
-    val colorHints = listOf(
-        Color(0xFFFF0000), // Red
-        Color(0xFF00FF00), // Green
-        Color(0xFF0000FF), // Blue
-        Color(0xFFFFFF00), // Yellow
-        Color(0xFF00FFFF), // Cyan
-        Color(0xFFFF00FF)  // Magenta
-    )
-
+    
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(40.dp)
+            .padding(10.dp)
     ) {
         Row(
             modifier = Modifier
                 .fillMaxSize()
         ) {
-
-
-            Box(
-                modifier = Modifier
-                    .padding(start = 10.dp, top = 30.dp, end = 20.dp)
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_add), // Replace with your actual resource ID
-                    contentDescription = "Add Color",
-                    modifier = Modifier
-                        .size(50.dp) // Adjust size as needed
-                        .clickable {
-                            // Handle the add color button click
-                            // Call your onAddColor() or any other logic here
-                            Log.i("SHERIF", "$selectedColor.value.toString()")
-                            onAddColor(selectedColor.value)
-                            Log.i("SHERIF", "Add color clicked")
-                        }
-                )
-            }
 
             io.mhssn.colorpicker.ColorPicker(
                 type = ColorPickerType.Ring(
@@ -99,22 +68,6 @@ fun ColorPickerContent(
                 Log.i("SHERIF", "RGB Value: R=$red, G=$green, B=$blue")
             }
 
-            //            HsvColorPicker(
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .height(150.dp)
-//                    .padding(10.dp),
-//                controller = controller,
-//                onColorChanged = { color ->
-//                    val red = (color.color.red * 255).toInt()
-//                    val green = (color.color.green * 255).toInt()
-//                    val blue = (color.color.blue * 255).toInt()
-//                    selectedColor.value = color.color
-//                    onColorChanged(color.color)
-//                    Log.i("SHERIF", "RGB Value: R=$red, G=$green, B=$blue")
-//                }
-//            )
-
 //            BrightnessSlider(
 //                modifier = Modifier
 //                    .fillMaxWidth()
@@ -122,45 +75,10 @@ fun ColorPickerContent(
 //                    .height(20.dp),
 //                controller = controller
 //            )
-            // Add Image Button (ic_add)
 
         }
 
 
-//        // Color Hints (Small squares on the right side)
-//        LazyColumn(
-//            modifier = Modifier
-//                .fillMaxHeight()
-//                .padding(end = 10.dp, top = 40.dp) // Adjust spacing from the right
-//                .align(Alignment.CenterEnd), // Position on the right side
-//            verticalArrangement = Arrangement.spacedBy(8.dp)
-//        ) {
-//            items(colorHints) { color ->
-//                ColorHintSquare(color = color, onColorSelected = {
-//                    controller.selectByColor(color,true)
-//                    selectedColor.value = color
-//                })
-//            }
-//        }
     }
 }
 
-
-//@Composable
-//fun ColorHintSquare(color: Color, onColorSelected: (Color) -> Unit) {
-//    Box(
-//        modifier = Modifier
-//            .size(15.dp) // Small square size
-//            .background(color)
-//            .clickable { onColorSelected(color) }
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun ColorPickerPreview() {
-//    // Pass a dummy ViewModel in the preview
-//}
-
-
-////////////
