42cf0baa6b5d7a5d2c57925bd4199e117396e5e7
Ahmed Essam <ahmed.e.mogy@gmail.com>
2024-12-02 18:33:51 +0200

    Update ColorPickerContent.kt

--- LED_Strip_Control_Application/app/src/main/java/com/example/led_strip_control/home/view/ColorPickerContent.kt
+++ LED_Strip_Control_Application/app/src/main/java/com/example/led_strip_control/home/view/ColorPickerContent.kt
@@ -28,6 +28,7 @@ import com.example.led_strip_control.R
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.draw.scale
 import io.mhssn.colorpicker.ColorPickerType
 
 
@@ -39,26 +40,30 @@ fun ColorPickerContent(
 ) {
     val controller = rememberColorPickerController()
     val selectedColor = remember { mutableStateOf(Color(1f, 0f, 0f)) } // Default red color
-    
+
     Box(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(10.dp)
+            .size(300.dp)
+            .padding(0.dp)
     ) {
         Row(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
         ) {
 
             io.mhssn.colorpicker.ColorPicker(
                 type = ColorPickerType.Ring(
-                    ringWidth = 30.dp,
+                    ringWidth = 35.dp,
                     previewRadius = 30.dp,
                     showDarknessBar = false,
                     showLightnessBar = false,
                     showColorPreview = true,
                     showAlphaBar = false,
-                )
+                ),
+                modifier = Modifier
+                    .scale(1.3f)
             ) { color ->
                 val red = (color.red * 255).toInt()
                 val green = (color.green * 255).toInt()
