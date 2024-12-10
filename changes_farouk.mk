0f4dbd0bc27617225a4830fe82753e72ce950682
Farouk157 <faroukehap90@gmail.com>
2024-12-10 09:27:44 +0200

    this is the make file to add our changes to the brcm RPI kit

---
+++ changes_farouk.mk
@@ -0,0 +1,20 @@
+PRODUCT_MODEL := RPI4 Customized by Farouk
+
+
+BOARD_SEPOLICY_DIRS += \
+    device/farouk/sepolicy \
+    device/farouk/sepolicy/example_service
+
+		
+
+PRODUCT_PACKAGES += i2c_service \
+                    i2c_service_user_app \                 
+                    ledstrip_service \
+                    strip_service_user_app
+
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += device/farouk/manifests/framework_compatibility_matrix.xml 
+
+
+PRODUCT_COPY_FILES += \
+    device/farouk/i2c_service/i2c_service.rc:vendor/etc/init/i2c_service.rc \
+    device/farouk/ledstrip_service/ledstrip_service.rc:vendor/etc/init/ledstrip_service.rc \
